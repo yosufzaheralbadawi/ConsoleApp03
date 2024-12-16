63a07cac9df43168d0c5e1ba3bf2035b53865d67
UseR <UseR@DESKTOP-C0A9EBU>
2024-12-16 22:57:07 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,238 @@
+﻿
+using System;
+
+class Program
+
+
+{
+    //1
+
+
+
+    //static void Main()
+    //{
+
+    //    Console.Write("Enter a numbers: ");
+    //    int number = int.Parse(Console.ReadLine());
+
+    //    if (number % 3 == 0 && number % 4 == 0)
+    //    {
+    //        Console.WriteLine("Yes");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("No");
+    //    }
+    //}
+
+
+
+
+    //2
+
+
+
+
+    //static void Main()
+    //{
+
+    //    Console.Write("Enter a number: ");
+    //    int number = int.Parse(Console.ReadLine());
+
+    //    if (number < 0)
+    //    {
+    //        Console.WriteLine("negative");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("positive");
+    //    }
+    //}
+
+
+    //3
+
+
+    //    static void Main(string[] args)
+    //{
+
+    //    Console.WriteLine("Enter 3 integers:");
+    //    int num1 = int.Parse(Console.ReadLine());
+    //    int num2 = int.Parse(Console.ReadLine());
+    //    int num3 = int.Parse(Console.ReadLine());
+
+
+    //    int max = Math.Max(num1, Math.Max(num2, num3));
+    //    int min = Math.Min(num1, Math.Min(num2, num3));
+
+
+    //    Console.WriteLine($"Max element = {max}");
+    //    Console.WriteLine($"Min element = {min}");
+    //}
+
+
+    //4
+
+
+
+
+    //static void Main()
+    //{
+
+    //    Console.Write("Enter a number: ");
+    //    int number = int.Parse(Console.ReadLine());
+
+    //    if (number % 2 == 0)
+    //    {
+    //        Console.WriteLine("Even");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("Odd");
+    //    }
+    //}
+
+
+
+    //5
+
+
+    //static void Main(string[] args)
+    //{
+
+    //    Console.Write("Enter a letter");
+    //    char character = Convert.ToChar(Console.ReadLine());
+
+    //    if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u')
+    //    {
+    //        Console.WriteLine("vowel");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("consonant");
+    //    }
+    //}
+
+
+
+    //6
+
+
+    //static void Main()
+    //{
+    //           Console.Write("Enter an integer ");
+    //    int number = int.Parse(Console.ReadLine());
+
+
+    //    Console.WriteLine("Multiplication table for" + number + ":");
+    //    for (int i = 1; i <= 12; i++)
+    //    {
+    //        Console.Write(number * i + " ");
+    //    }
+    //}
+
+
+    //7
+
+
+
+    //static void Main()
+    //{
+
+    //    Console.Write("Enter a number: ");
+    //    int number = int.Parse(Console.ReadLine());
+
+    //    Console.WriteLine("Even numbers between 1 and " + number + ":");
+    //    for (int i = 2; i <= number; i += 2)
+    //    {
+    //        Console.Write(i + " ");
+    //    }
+    //}
+
+
+
+    //8  
+
+
+    //static void Main()
+    //{
+    //    Console.WriteLine("Enter the base number: ");
+    //    int baseNumber = int.Parse(Console.ReadLine());
+    //    Console.Write("Enter the exponent: ");
+    //    int exponent = int.Parse(Console.ReadLine());
+    //    int result = (int)Math.Pow(baseNumber, exponent);
+    //    Console.WriteLine("The result is: " + result);
+    //}
+
+
+    //9
+
+
+    //static void Main()
+    //{
+
+    //    Console.Write("Enter the month number: ");
+    //    int monthNumber = int.Parse(Console.ReadLine());
+
+
+    //    int daysInMonth;
+
+    //    if (monthNumber == 1 || monthNumber == 3 || monthNumber == 5 || monthNumber == 7 || monthNumber == 8 || monthNumber == 10 || monthNumber == 12)
+    //    {
+    //        daysInMonth = 31;
+    //    }
+    //    else if (monthNumber == 4 || monthNumber == 6 || monthNumber == 9 || monthNumber == 11)
+    //    {
+    //        daysInMonth = 30;
+    //    }
+    //    else if (monthNumber == 2)
+    //    {
+    //        daysInMonth = 28;  
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("Invalid month number.");
+    //        return;
+    //    }
+
+
+    //    Console.WriteLine("Days in the month: " + daysInMonth);
+    //}
+
+
+    //10
+
+
+    //static void Main()
+    //{
+        
+    //    Console.Write("Enter the time taken to complete the task (in hours): ");
+    //    double timeTaken = double.Parse(Console.ReadLine());
+
+        
+    //    if (timeTaken >= 2 && timeTaken <= 3)
+    //    {
+    //        Console.WriteLine("The worker is highly efficient.");
+    //    }
+    //    else if (timeTaken > 3 && timeTaken <= 4)
+    //    {
+    //        Console.WriteLine("The worker is instructed to increase their speed.");
+    //    }
+    //    else if (timeTaken > 4 && timeTaken <= 5)
+    //    {
+    //        Console.WriteLine("The worker is provided with training to enhance their speed.");
+    //    }
+    //    else if (timeTaken > 5)
+    //    {
+    //        Console.WriteLine("The worker is required to leave the company.");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("Invalid input. Please enter a valid time greater than 0.");
+    //    }
+    //}
+
+}
+
+
+
